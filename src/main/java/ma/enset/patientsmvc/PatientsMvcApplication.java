de863b4b13a2ad2648efb808d73f8896ecf7b3cc
Hossam Onadja <hossamonadja@gmial.com>
2022-04-25 22:51:45 +0000

    First Commit

---
+++ src/main/java/ma/enset/patientsmvc/PatientsMvcApplication.java
@@ -0,0 +1,32 @@
+package ma.enset.patientsmvc;
+
+import ma.enset.patientsmvc.entities.Patient;
+import ma.enset.patientsmvc.repositories.PatientRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
+
+@SpringBootApplication
+public class PatientsMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatientsMvcApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(PatientRepository patientRepository){
+        return args -> {
+            patientRepository.save(new Patient(null, "Hossam", new Date(), false, 12));
+            patientRepository.save(new Patient(null, "Hassan", new Date(), true, 321));
+            patientRepository.save(new Patient(null, "Manal", new Date(), true, 18));
+            patientRepository.save(new Patient(null, "yassine", new Date(), false, 32));
+
+            patientRepository.findAll().forEach(p -> {
+                System.out.println(p.getNom());
+            });
+        };
+    }
+}
